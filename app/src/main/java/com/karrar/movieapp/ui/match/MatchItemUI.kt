26297349464117fb7e548f425263e2fa95b54af6
NadeenXo <nadeenserag@gmail.com>
2025-09-13 23:24:28 +0300

    start of the pager

---
+++ app/src/main/java/com/karrar/movieapp/ui/match/MatchItemUI.kt
@@ -0,0 +1,23 @@
+package com.karrar.movieapp.ui.match
+
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import coil.Coil
+
+data class MatchItemUI(
+    val id: Int,
+    val title: String?,
+    val posterUrl: String?,
+    val backdropUrl: String?,
+    val genres: String?,            // "Drama, Action, Thriller"
+    val voteAverage: Double?,       // 8.5
+    val runtimeFormatted: String?,  // "2h 32m"
+    val releaseDateShort: String?   // "2008, Jul 18"
+)
+
+interface MatchResultCallbacks {
+    fun onPlay(item: MatchItemUI)
+    fun onSave(item: MatchItemUI)
+    fun onDetails(item: MatchItemUI)
+}
