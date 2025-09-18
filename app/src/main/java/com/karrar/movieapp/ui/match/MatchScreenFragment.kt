0903a92ab276f965ab8b74acf189c9f34b9cedf3
Asmaa Karam <asmaakaram793@gmai.com>
2025-09-18 06:18:15 +0300

    add discover match screen xml

---
+++ app/src/main/java/com/karrar/movieapp/ui/match/MatchScreenFragment.kt
@@ -0,0 +1,22 @@
+package com.karrar.movieapp.ui.match
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.karrar.movieapp.R
+import com.karrar.movieapp.databinding.FragmentMatchScreenBinding
+import com.karrar.movieapp.ui.base.BaseFragment
+
+class MatchScreenFragment(): BaseFragment<FragmentMatchScreenBinding> (){
+    override val layoutIdFragment: Int = R.layout.fragment_match_screen
+    override val viewModel: MatchScreenViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setTitle(true, getString(R.string.discover_your_match))
+        binding.startButton.setOnClickListener {
+            findNavController().navigate(R.id.action_matchFragment_to_matchQuestionFragment)
+        }
+    }
+}
