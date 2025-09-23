2b90396a0010e6b624bf3a73bb70006c15d2f9ad
Asmaa Karam <asmaakaram793@gmai.com>
2025-09-23 22:52:28 +0300

    update RatedMoviesMapper to get releaseDate, geners and duration

--- app/src/main/java/com/karrar/movieapp/domain/mappers/movie/RatedMoviesMapper.kt
+++ app/src/main/java/com/karrar/movieapp/domain/mappers/movie/RatedMoviesMapper.kt
@@ -5,6 +5,7 @@ import com.karrar.movieapp.data.remote.response.RatedMoviesDto
 import com.karrar.movieapp.domain.mappers.Mapper
 import com.karrar.movieapp.domain.models.Rated
 import com.karrar.movieapp.utilities.Constants
+import com.karrar.movieapp.utilities.convertToDayMonthYearFormat
 import javax.inject.Inject
 
 class RatedMoviesMapper @Inject constructor() : Mapper<RatedMoviesDto, Rated> {
@@ -14,9 +15,11 @@ class RatedMoviesMapper @Inject constructor() : Mapper<RatedMoviesDto, Rated> {
             title = input.title ?: "",
             posterPath = BuildConfig.IMAGE_BASE_PATH + input.backdropPath,
             rating = input.rating ?: 0F,
-            releaseDate = input.releaseDate ?: "",
+            releaseDate = input.releaseDate?.convertToDayMonthYearFormat()?: "",
             mediaType = Constants.MOVIE,
-            voteAverage = input.voteAverage ?: 0.0
+            voteAverage = input.voteAverage ?: 0.0,
+            genres = input.genreIds?.filterNotNull() ?: emptyList(),
+            duration = ""
         )
     }
 }
