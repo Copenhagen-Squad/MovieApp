c9a358ec46bdf4f557d61c801b41937bc6aa0982
Asmaa Karam <asmaakaram793@gmai.com>
2025-09-23 22:56:41 +0300

    update view model to handel tab click and update date

--- app/src/main/java/com/karrar/movieapp/ui/profile/myratings/MyRatingsViewModel.kt
+++ app/src/main/java/com/karrar/movieapp/ui/profile/myratings/MyRatingsViewModel.kt
@@ -1,10 +1,15 @@
 package com.karrar.movieapp.ui.profile.myratings
 
 import androidx.lifecycle.viewModelScope
+import com.karrar.movieapp.domain.models.Rated
+import com.karrar.movieapp.domain.usecases.GetGenreListUseCase
 import com.karrar.movieapp.domain.usecases.GetListOfRatedUseCase
+import com.karrar.movieapp.domain.usecases.movieDetails.GetMovieDetailsUseCase
+import com.karrar.movieapp.domain.usecases.tvShowDetails.GetTvShowDetailsUseCase
 import com.karrar.movieapp.ui.base.BaseViewModel
 import com.karrar.movieapp.utilities.Constants
 import com.karrar.movieapp.utilities.Event
+import com.karrar.movieapp.utilities.formatRuntime
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -12,21 +17,27 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-import okhttp3.Dispatcher
 import javax.inject.Inject
 
 @HiltViewModel
 class MyRatingsViewModel @Inject constructor(
     private val getRatedUseCase: GetListOfRatedUseCase,
-    private val ratedUIStateMapper: RatedUIStateMapper
+    private val ratedUIStateMapper: RatedUIStateMapper,
+    private val getGenreListUseCase: GetGenreListUseCase,
+    private val getMovieDetailsUseCase: GetMovieDetailsUseCase,
+    private val getTvShowDetailsUseCase: GetTvShowDetailsUseCase
 ) : BaseViewModel(), RatedMoviesInteractionListener {
 
     private val _ratedUiState = MutableStateFlow(MyRateUIState())
     val ratedUiState: StateFlow<MyRateUIState> = _ratedUiState
-
     private val _myRatingUIEvent: MutableStateFlow<Event<MyRatingUIEvent?>> =
         MutableStateFlow(Event(null))
     val myRatingUIEvent = _myRatingUIEvent.asStateFlow()
+
+    private val _currentTab = MutableStateFlow(Constants.MOVIE)
+
+    private var allRatedList: List<RatedUIState> = emptyList()
+
 
     init {
         getData()
@@ -36,33 +47,74 @@ class MyRatingsViewModel @Inject constructor(
         viewModelScope.launch(Dispatchers.IO) {
             _ratedUiState.update { it.copy(isLoading = true) }
             try {
-                val listOfRatedMovies =
-                    getRatedUseCase.getRatedMovie()
-                        .map { rate -> ratedUIStateMapper.map(rate) }
+                val ratedList = getRatedUseCase()
 
-                val listOfRatedTvShows =
-                    getRatedUseCase.getRatedTvShow() // series
-                        .map { rate -> ratedUIStateMapper.map(rate) }
+                val movieGenresMap = getGenreListUseCase(Constants.MOVIE_CATEGORIES_ID)
+                    .associateBy { it.genreID }
+                val seriesGenresMap = getGenreListUseCase(Constants.TV_CATEGORIES_ID)
+                    .associateBy { it.genreID }
 
-                _ratedUiState.update { it.copy(rateMovies = listOfRatedMovies, ratedSeries = listOfRatedTvShows, isLoading = false) }
+                val ratedListWithDetails = addMovieDetailsData(ratedList)
+
+                allRatedList = ratedListWithDetails.map { rated ->
+                    val baseUi = ratedUIStateMapper.map(rated)
+                    val genresNames = when (rated.mediaType) {
+                        Constants.MOVIE -> rated.genres.mapNotNull { movieGenresMap[it]?.genreName }
+                        Constants.TV_SHOWS -> rated.genres.mapNotNull { seriesGenresMap[it]?.genreName }
+                        else -> emptyList()
+                    }
+
+                    baseUi.copy(
+                        genres = genresNames,
+                        duration = rated.duration
+                    )
+                }
+
+                filterRatedList()
+                _ratedUiState.update { it.copy(isLoading = false) }
             } catch (e: Throwable) {
                 _ratedUiState.update { it.copy(error = listOf(Error("")), isLoading = false) }
             }
         }
     }
 
+    fun selectTab(tab: String) {
+        _currentTab.value = tab
+        filterRatedList()
+    }
+
+    private fun filterRatedList() {
+        val filtered = allRatedList.filter { it.mediaType == _currentTab.value }
+        _ratedUiState.update { it.copy(ratedList = filtered) }
+    }
 
     override fun onClickMovie(movieId: Int) {
-//        ratedUiState.value.ratedList.let { it ->
-//            val item = it.find { it.id == movieId }
-//            item?.let {
-//                if (it.mediaType == Constants.MOVIE) {
-//                    _myRatingUIEvent.update { Event(MyRatingUIEvent.MovieEvent(movieId)) }
-//                } else {
-//                    _myRatingUIEvent.update { Event(MyRatingUIEvent.TVShowEvent(movieId)) }
-//                }
-//            }
-//        }
+        ratedUiState.value.ratedList.let { it ->
+            val item = it.find { it.id == movieId }
+            item?.let {
+                if (it.mediaType == Constants .MOVIE) {
+                    _myRatingUIEvent.update { Event(MyRatingUIEvent.MovieEvent(movieId)) }
+                } else {
+                    _myRatingUIEvent.update { Event(MyRatingUIEvent.TVShowEvent(movieId)) }
+                }
+            }
+        }
+    }
+
+    private suspend fun addMovieDetailsData(ratedItems: List<Rated>): List<Rated> {
+        return ratedItems.map { rate ->
+            if (rate.mediaType == Constants.MOVIE) {
+                val movieDetails = getMovieDetailsUseCase.getMovieDetails(rate.id)
+                rate.copy(
+                    duration = movieDetails.movieDuration.formatRuntime().toString(),
+                )
+            } else {
+                val tvDetails = getTvShowDetailsUseCase.getTvShowDetails(rate.id)
+                rate.copy(
+                    duration = tvDetails.tvShowEpisodeRuntime.firstOrNull().formatRuntime().toString()
+                )
+            }
+        }
     }
 
     fun retryConnect() {
