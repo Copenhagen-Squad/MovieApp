e333e52d183a7463cbeb0b213a61d5f72f6d5078
Asmaa Karam <asmaakaram793@gmai.com>
2025-09-26 05:43:43 +0300

    create adapter for my featured collection

---
+++ app/src/main/java/com/karrar/movieapp/ui/home/adapter/FeaturesCollectionAdapter.kt
@@ -0,0 +1,21 @@
+package com.karrar.movieapp.ui.home.adapter
+
+import com.karrar.movieapp.ui.base.BaseAdapter
+import com.karrar.movieapp.ui.base.BaseInteractionListener
+import com.karrar.movieapp.ui.home.homeUiState.FeaturedCollectionUiState
+import com.karrar.movieapp.ui.home.homeUiState.FeaturedCollectionsTarget
+import com.karrar.movieapp.R
+
+class FeaturesCollectionAdapter(
+    items: List<FeaturedCollectionUiState>, val listener: FeaturedCollectionListener,
+
+    ) : BaseAdapter<FeaturedCollectionUiState>(items, listener) {
+
+    override val layoutID: Int = R.layout.collection_item
+
+}
+
+interface FeaturedCollectionListener : BaseInteractionListener {
+    fun onClickFeaturedCollections(target: FeaturedCollectionsTarget)
+
+}
